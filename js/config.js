db78a0c8e0e7047b17d220a34b40a3dfcd2e4a72
Arnoud de Boer <a.deboer@geonovum.nl>
2024-05-06 09:32:19 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,8 +7,8 @@ let respecConfig = {
   title: "IMX - Metadata basic principles",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   //specStatus: "wv",                   // Werkversie
-  specStatus: "wv",               // Consultatieversie
-  //specStatus: "vv",               // Versie ter vaststelling
+  //specStatus: "wv",               // Consultatieversie
+  specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
@@ -32,7 +32,7 @@ let respecConfig = {
   shortName: "Metadata",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2024-01-08",
+  publishDate: "2024-05-01",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
